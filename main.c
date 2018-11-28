248a5fbf98870cb9ac9572d1b0b4e6468f33c717
Caskey, Damon V <dc@caskeys.com>
2018-11-28 11:32:24 -0500

    Clean up main.c file.

--- main.c
+++ main.c
@@ -6,18 +6,6 @@
     Caskey, Damon V.
     2016-03-17
 
-    Source license: http://www.caskeys.com/dc/?p=5067
-
-    Adds secondary jumping actions on key press.
-
-    To install, add the dc_hansburg folder to your Scripts folder.
-
-    You will then need to include this file (main.c) into your key
-    scripts (#include "data/scripts/dc_hansburg/main.c).
-
-    Once included, you can then call the functions within your key script.
-
-    ***Function list***
-
-    dc_hansburg()
+    https://github.com/DCurrent/openbor-script-Hansburg
+/*
 
