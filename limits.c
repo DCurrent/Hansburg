8045049c9ed773b24a3b05520ecdfa3fb5857977
Caskey, Damon V <dc@caskeys.com>
2019-04-14 23:09:34 -0400

    Update limit variable control and timer verify functions.

--- limits.c
+++ limits.c
@@ -2,12 +2,12 @@
 
 int dc_hansburg_get_max_height()
 {
-	int instance;
+	char id;
 	int result; 
 	
-	instance = dc_hansburg_get_instance();
+	id = dc_hansburg_get_instance() + DC_HANSBURG_VAR_KEY_MAX_HEIGHT;
 
-	result = getlocalvar(instance + DC_HANSBURG_VAR_KEY_MAX_HEIGHT);
+	result = getlocalvar(id);
 
 	if (typeof(result) != openborconstant("VT_INTEGER"))
 	{
@@ -19,67 +19,74 @@ int dc_hansburg_get_max_height()
 
 void dc_hansburg_set_max_height(int value)
 {
-	int instance;
+	int id;
 
-	// Get instance.
-	instance = dc_hansburg_get_instance();
+	// Get id.
+	id = dc_hansburg_get_instance() + DC_HANSBURG_VAR_KEY_MAX_HEIGHT;
+
+	if (value == DC_HANSBURG_DEFAULT_MAX_HEIGHT)
+	{
+		value = NULL();
+	}
 
 	// Set value.
-	setlocalvar(instance + DC_HANSBURG_VAR_KEY_MAX_HEIGHT, value);
+	setlocalvar(id, value);
+}
+
+int dc_hansburg_get_disable_time()
+{
+	char id;
+	int result;
+
+	// Get id.
+	id = dc_hansburg_get_instance() + DC_HANSBURG_VAR_KEY_DISABLE;
+
+	result = getlocalvar(id);
+
+	if (typeof(result) != openborconstant("VT_INTEGER"))
+	{
+		result = DC_HANSBURG_DEFAULT_DISABLE
+	}
+	
+	return result;
 }
 
 void dc_hansburg_set_disable_time(int value)
 {
-	int vartype = NULL();
+	char id;
 
-	vartype = typeof(value);
+	// Get id.
+	id = dc_hansburg_get_instance() + DC_HANSBURG_VAR_KEY_DISABLE;
 
-	// Catch bad arguments here.
-	if (vartype != openborconstant("VT_INTEGER")
-		|| vartype != openborconstant("VT_EMPTY"))
+	if (value == DC_HANSBURG_DEFAULT_DISABLE)
 	{
-		log("\n\n error: dc_hansburg_set_disable_time(float value): {value} argument has invalid type. Integer or empty (NULL) value required.");
-		return;
+		value = NULL();
 	}
 
-	setindexedvar(DC_HANSBURG_VAR_KEY_DISABLE, value);
+	setindexedvar(id, value);
 }
 
 // Return true if disable time set by user. Clears timer
 // if expired and not infinite.
 int dc_hansburg_disable_check()
 {
-	int result = 0;
-	int elapsed_time = 0;
-	int time_disable = 0;
-	int vartype = 0;
-
-	time_disable = getindexedvar(DC_HANSBURG_VAR_KEY_DISABLE);
-
-	vartype = typeof(time_disable);
-
-	// Does time have any valid integer value?
-	if (vartype == openborconstant("VT_INTEGER"))
+	int result;
+	int elapsed_time;
+	int time_disable;
+	
+	time_disable = dc_hansburg_get_disable_time();
+	elapsed_time = openborvariant("elapsed_time");
+	
+	// Is timer still active (greater than
+	// current elapsed time) or infinite? if either
+	// is true, than we return true.
+	if (time_disable > elapsed_time
+		|| time_disable != DC_HANSBURG_TIME_INFINITE)
 	{
-		// Is timer still active (greater than
-		// current elapsed time) or infinite? if either
-		// is true, than we return true. Elsewise
-		// we should clear the timer and
-		// do nothing. A default of false
-		// will be returned instead.
-		if (time_disable > elapsed_time
-			|| time_disable != DC_HANSBURG_TIME_INFINITE)
-		{
-			// Result will be true.
-			result = 1;
-		}
-		else
-		{
-			time_disable = NULL();
-
-			setindexedvar(DC_HANSBURG_VAR_KEY_DISABLE, time_disable);
-		}
+		// Return true.
+		return 1;
 	}
-
-	return result;
+	
+	// Return false.
+	return 0;
 }
